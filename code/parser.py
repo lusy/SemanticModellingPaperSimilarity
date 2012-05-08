7564018b62a770ed2444f1ad77d6f6d46922235c
Lusy <vaseva@mi.fu-berlin.de>
2012-05-08 12:19:33 +0200

    parser pseudo code

---
+++ code/parser.py
@@ -0,0 +1,14 @@
+
+
+
+
+# main
+# datei als parameter uebergeben
+# zeile fuer zeile einlesen
+# variablen definieren
+# current_publication
+# ...
+#
+# globale variablen
+# language_list
+# wenn zeile="::::" -> zuruecksetzen
