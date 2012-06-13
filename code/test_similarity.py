0d8a99160c709c465982848f4d34fdc5a26258ea
Lusy <vaseva@mi.fu-berlin.de>
2012-06-13 07:37:45 +0200

    it computes something^^

--- code/test_similarity.py
+++ code/test_similarity.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import numpy
 
-G = nx.DiGraph()
+G = nx.Graph()
 
 G.add_nodes_from([('Publication_1', {'Class':'Publication'}), ('Publication_2', {'Class':'Publication'}), ('Keyword_1', {'Class':'Keyword'}), ('Keyword_2',{'Class':'Keyword'}), ('Keyword_3', {'Class':'Keyword'})])
 
@@ -13,9 +13,19 @@ sim_key = dict()
 l1 = 0.5 #lambda fuer publications
 l2 = 0.5 #lambda fuer keywords
 c = 0.8 #damping factor
-k = 5
+k =10
 
 #initialization
+for a in G.nodes_iter():
+    if G.node[a]['Class'] == 'Publication':
+        sim_pub[a]={}
+    elif G.node[a]['Class'] == 'Keyword':
+        sim_key[a]={}
+    else:
+        pass
+        #there are only publications and keywords in the current graph
+
+
 for a in G.nodes_iter():
     for b in G.nodes_iter():
         if G.node[a]['Class'] == 'Publication' and G.node[b]['Class'] == 'Publication':
@@ -23,61 +33,88 @@ for a in G.nodes_iter():
                 sim_pub[a][b] = 1
             else:
                 sim_pub[a][b] = 0
-                sim_pub[b][a] = 0
         elif G.node[a]['Class'] == 'Keyword' and G.node[b]['Class'] == 'Keyword':
             if a==b:
                 sim_key[a][b] = 1
             else:
                 sim_key[a][b] = 0
-                sim_key[b][a] = 0
         else:
             pass
             # a and b belong to different classes and are dissimilar
 
+#print("sim_pub: ", sim_pub)
+#print("sim_key: ", sim_key)
 
 #iteration
 while k>0:
     k = k-1
     for a in G.nodes_iter():
         for b in G.nodes_iter():
-            if G.node[a]['Class']=='Publication' and G.node[b]['Class'] == 'Publication':
-                pubValue = 0
-                publicationNeighborsOfA = list()
-                publicationNeighborsOfB = list()
-                for na in G.neighbors_iter(a) and G.node[na]['Class'] =='Publication':
-                    publicationNeighborsOfA.append(na)
-                    for nb in G.neighbors_iter(b) and G.node[nb]['Class'] == 'Publication':
-                        publicationNeighborsOfB.append(nb)
-                        pubValue = pubValue + sim_pub[na][nb]
+            if a!=b: #otherwise is similarity = 1
+                if G.node[a]['Class']=='Publication' and G.node[b]['Class'] == 'Publication':
+                    pubValue = 0
+                    publicationNeighborsOfA = list()
+                    publicationNeighborsOfB = list()
+                    for na in G.neighbors_iter(a):
+                        if G.node[na]['Class'] =='Publication':
+                            publicationNeighborsOfA.append(na)
+                            for nb in G.neighbors_iter(b):
+                                if G.node[nb]['Class'] == 'Publication':
+                                    publicationNeighborsOfB.append(nb)
+                                    print("Is pubValue 0? ", pubValue)
+                                    pubValue = pubValue + sim_pub[na][nb]
 
-                keyPubValue = 0
-                keywordNeighborsOfA = list()
-                keywordNeighborsOfB = list()
-                for na in G.neighbors_iter(a) and G.node[na]['Class'] == 'Keyword':
-                    keywordNeighborsOfA.append(na)
-                    for nb in G.neighbors_iter(b) and G.node[nb]['Class']== 'Keyword':
-                        keywordNeighborsOfB.append(nb)
-                        keyPubValue = keyPubValue + sim_key[na][nb]
+                    keyPubValue = 0
+                    keywordNeighborsOfA = list()
+                    keywordNeighborsOfB = list()
+                    for nka in G.neighbors_iter(a):
+                        if G.node[nka]['Class'] == 'Keyword':
+                            keywordNeighborsOfA.append(nka)
+                            for nkb in G.neighbors_iter(b):
+                                if G.node[nkb]['Class']== 'Keyword':
+                                    keywordNeighborsOfB.append(nkb)
+                                    keyPubValue = keyPubValue + sim_key[na][nkb]
 
-                sim_pub[a][b] = l1*c*pubValue / (len(publicationNeighborsOfA)*len(publicationNeighborsOfB)) + l2*c*keyPubValue / (len(keywordNeighborsOfA)*len(keywordNeighborsOfB))
+                    print("Debugging...............")
+                    print("a: ", a)
+                    print("b: ", b)
+                    print('neighbors of a: ', G.neighbors(a))
+                    print('neighbors of b: ', G.neighbors(b))
+                    print('pubvalue: ', pubValue)
+                    print('publicationNeighborsOfA: ', publicationNeighborsOfA)
+                    print('publicationNeighborsOfB: ', publicationNeighborsOfB)
+                    print('keyPubValue: ', keyPubValue)
+                    print('keywordNeighborsOfA: ', keywordNeighborsOfA)
+                    print('keywordNeighborsOfB: ', keywordNeighborsOfB)
+                    sim_pub[a][b] = l1*c*pubValue / (len(set(publicationNeighborsOfA))*len(set(publicationNeighborsOfB))) + l2*c*keyPubValue / (len(set(keywordNeighborsOfA))*len(set(keywordNeighborsOfB)))
+                    print('sim_pub: ', sim_pub[a][b])
 
-            elif G.node[a]['Class'] == 'Keyword' and G.node[b]['Class'] == 'Keyword':
-                keywordValue = 0
-                neighborsOfA = list()
-                neighborsOfB = list()
-                for na in neighborsOfA:
-                    neighborsOfA.append(na)
-                    for nb in neighborsOfB:
-                        neighborsOfB.append(nb)
-                        keywordValue = keywordValue + sim_pub[na][nb] # keyword have publications as neighbors
+                elif G.node[a]['Class'] == 'Keyword' and G.node[b]['Class'] == 'Keyword':
+                    keywordValue = 0
+                    neighborsOfA = list()
+                    neighborsOfB = list()
+                    for na in G.neighbors_iter(a):
+                        neighborsOfA.append(na)
+                        for nb in G.neighbors_iter(b):
+                            neighborsOfB.append(nb)
+                            keywordValue = keywordValue + sim_pub[na][nb] # keyword have publications as neighbors
 
-                sim_key[a][b] = c*keywordValue / (len(neighborsOfA)*len(neighborsOfB))
+                    print("Debuging key..................")
+                    print('a: ', a)
+                    print('b: ', b)
+                    print('neighbors of a: ', G.neighbors(a))
+                    print('neighbors of b: ', G.neighbors(b))
+                    print('keywordvalue: ', keywordValue)
+                    print('NeighborsOfA: ', neighborsOfA)
+                    print('NeighborsOfB: ', neighborsOfB)
 
-            else:
-                pass
-                #do nothing, both nodes belong to different classes
+                    sim_key[a][b] = c*keywordValue / (len(set(neighborsOfA))*len(set(neighborsOfB)))
+
+                else:
+                    pass
+                    #do nothing, both nodes belong to different classes
 
     print('Next iteration')
-    print (sim_pub)
-    print (sim_key)
+    print ('sim_pub is: ', sim_pub)
+    print ('sim_key is: ', sim_key)
 
