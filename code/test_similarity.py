515fd7fb4c22fb6129e83987662d0f1e4450cef3
Lusy <vaseva@mi.fu-berlin.de>
2012-06-13 08:16:40 +0200

    play with different graphs

--- code/test_similarity.py
+++ code/test_similarity.py
@@ -3,17 +3,31 @@ import numpy
 
 G = nx.Graph()
 
-G.add_nodes_from([('Publication_1', {'Class':'Publication'}), ('Publication_2', {'Class':'Publication'}), ('Keyword_1', {'Class':'Keyword'}), ('Keyword_2',{'Class':'Keyword'}), ('Keyword_3', {'Class':'Keyword'})])
+###### Graph Nr 1 ###############################################
+#G.add_nodes_from([('Publication_1', {'Class':'Publication'}), ('Publication_2', {'Class':'Publication'}), ('Keyword_1', {'Class':'Keyword'}), ('Keyword_2',{'Class':'Keyword'}), ('Keyword_3', {'Class':'Keyword'})])
 
-G.add_edges_from([('Publication_1', 'Keyword_1', {'Relation':'hasKeyword'}), ('Publication_1', 'Keyword_2', {'Relation':'hasKeyword'}), ('Publication_1', 'Keyword_3', {'Relation':'hasKeyword'}), ('Publication_2', 'Keyword_2', {'Relation':'hasKeyword'}), ('Publication_2', 'Keyword_3', {'Relation':'hasKeyword'}), ('Publication_1', 'Publication_2', {'Relation':'cites'})])
+#G.add_edges_from([('Publication_1', 'Keyword_1', {'Relation':'hasKeyword'}), ('Publication_1', 'Keyword_2', {'Relation':'hasKeyword'}), ('Publication_1', 'Keyword_3', {'Relation':'hasKeyword'}), ('Publication_2', 'Keyword_2', {'Relation':'hasKeyword'}), ('Publication_2', 'Keyword_3', {'Relation':'hasKeyword'}), ('Publication_1', 'Publication_2', {'Relation':'cites'})])
+#################################################################
+
+###### Graph Nr 2 ###############################################
+#G.add_nodes_from([('Publication_1', {'Class':'Publication'}), ('Publication_2', {'Class':'Publication'}), ('Keyword_1', {'Class':'Keyword'}), ('Keyword_2',{'Class':'Keyword'}), ('Keyword_3', {'Class':'Keyword'})])
+
+#G.add_edges_from([('Publication_1', 'Keyword_1', {'Relation':'hasKeyword'}), ('Publication_2', 'Keyword_2', {'Relation':'hasKeyword'}), ('Publication_2', 'Keyword_3', {'Relation':'hasKeyword'}), ('Publication_1', 'Publication_2', {'Relation':'cites'})])
+#################################################################
+
+###### Graph Nr 3 ###############################################
+G.add_nodes_from([('Publication_1', {'Class':'Publication'}), ('Publication_2', {'Class':'Publication'}), ('Publication_3', {'Class':'Publication'})])
+
+G.add_edges_from([('Publication_3', 'Publication_1', {'Relation':'cites'}), ('Publication_2', 'Publication_3', {'Relation':'cites'}), ('Publication_1', 'Publication_2', {'Relation':'cites'})])
+#################################################################
 
 sim_pub = dict()
 sim_key = dict()
 
-l1 = 0.5 #lambda fuer publications
-l2 = 0.5 #lambda fuer keywords
-c = 0.8 #damping factor
-k =10
+l1 = 0.7 #lambda fuer publications
+l2 = 0.3 #lambda fuer keywords
+c = 0.7 #damping factor
+k = 12
 
 #initialization
 for a in G.nodes_iter():
@@ -61,7 +75,6 @@ while k>0:
                             for nb in G.neighbors_iter(b):
                                 if G.node[nb]['Class'] == 'Publication':
                                     publicationNeighborsOfB.append(nb)
-                                    print("Is pubValue 0? ", pubValue)
                                     pubValue = pubValue + sim_pub[na][nb]
 
                     keyPubValue = 0
@@ -75,19 +88,24 @@ while k>0:
                                     keywordNeighborsOfB.append(nkb)
                                     keyPubValue = keyPubValue + sim_key[na][nkb]
 
-                    print("Debugging...............")
-                    print("a: ", a)
-                    print("b: ", b)
-                    print('neighbors of a: ', G.neighbors(a))
-                    print('neighbors of b: ', G.neighbors(b))
-                    print('pubvalue: ', pubValue)
-                    print('publicationNeighborsOfA: ', publicationNeighborsOfA)
-                    print('publicationNeighborsOfB: ', publicationNeighborsOfB)
-                    print('keyPubValue: ', keyPubValue)
-                    print('keywordNeighborsOfA: ', keywordNeighborsOfA)
-                    print('keywordNeighborsOfB: ', keywordNeighborsOfB)
-                    sim_pub[a][b] = l1*c*pubValue / (len(set(publicationNeighborsOfA))*len(set(publicationNeighborsOfB))) + l2*c*keyPubValue / (len(set(keywordNeighborsOfA))*len(set(keywordNeighborsOfB)))
-                    print('sim_pub: ', sim_pub[a][b])
+                    #print("Debugging...............")
+                    #print("a: ", a)
+                    #print("b: ", b)
+                    #print('neighbors of a: ', G.neighbors(a))
+                    #print('neighbors of b: ', G.neighbors(b))
+                    #print('pubvalue: ', pubValue)
+                    #print('publicationNeighborsOfA: ', publicationNeighborsOfA)
+                    #print('publicationNeighborsOfB: ', publicationNeighborsOfB)
+                    #print('keyPubValue: ', keyPubValue)
+                    #print('keywordNeighborsOfA: ', keywordNeighborsOfA)
+                    #print('keywordNeighborsOfB: ', keywordNeighborsOfB)
+
+                    if keywordNeighborsOfA == [] or keywordNeighborsOfB == []:
+                        sim_pub[a][b] = l1*c*pubValue / (len(set(publicationNeighborsOfA))*len(set(publicationNeighborsOfB)))
+                    else:
+                        sim_pub[a][b] = l1*c*pubValue / (len(set(publicationNeighborsOfA))*len(set(publicationNeighborsOfB))) + l2*c*keyPubValue / (len(set(keywordNeighborsOfA))*len(set(keywordNeighborsOfB)))
+
+                    #print('sim_pub: ', sim_pub[a][b])
 
                 elif G.node[a]['Class'] == 'Keyword' and G.node[b]['Class'] == 'Keyword':
                     keywordValue = 0
@@ -99,16 +117,18 @@ while k>0:
                             neighborsOfB.append(nb)
                             keywordValue = keywordValue + sim_pub[na][nb] # keyword have publications as neighbors
 
-                    print("Debuging key..................")
-                    print('a: ', a)
-                    print('b: ', b)
-                    print('neighbors of a: ', G.neighbors(a))
-                    print('neighbors of b: ', G.neighbors(b))
-                    print('keywordvalue: ', keywordValue)
-                    print('NeighborsOfA: ', neighborsOfA)
-                    print('NeighborsOfB: ', neighborsOfB)
+                    #print("Debuging key..................")
+                    #print('a: ', a)
+                    #print('b: ', b)
+                    #print('neighbors of a: ', G.neighbors(a))
+                    #print('neighbors of b: ', G.neighbors(b))
+                    #print('keywordvalue: ', keywordValue)
+                    #print('NeighborsOfA: ', neighborsOfA)
+                    #print('NeighborsOfB: ', neighborsOfB)
 
                     sim_key[a][b] = c*keywordValue / (len(set(neighborsOfA))*len(set(neighborsOfB)))
+
+                    #print('sim_key: ', sim_key[a][b])
 
                 else:
                     pass
@@ -117,4 +137,4 @@ while k>0:
     print('Next iteration')
     print ('sim_pub is: ', sim_pub)
     print ('sim_key is: ', sim_key)
-
+    print('\n')
