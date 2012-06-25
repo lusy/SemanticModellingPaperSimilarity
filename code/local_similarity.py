fea780c0db8a4ac0212c3340021b1746d139267d
Lusy <vaseva@mi.fu-berlin.de>
2012-06-25 22:39:24 +0200

    routine for computing all paths between two nodes added

--- code/local_similarity.py
+++ code/local_similarity.py
@@ -2,12 +2,27 @@ import networkx as nx
 import timeit
 import sys
 
+def find_all_paths(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return [path]
+    if not graph.has_node(start):
+        return []
+    paths = []
+    for node in graph[start]:
+        if node not in path:
+            newpaths = find_all_paths(graph, node, end, path)
+            for newpath in newpaths:
+                paths.append(newpath)
+    return paths
+
 def main(args):
     G = nx.read_graphml('../data/testdata/head200_parsedcomplete_graphml.graphml')
 
     sim_pub = dict()
 
     t = 10 #maximum depth
+    d = 0.04 #decay factor
     # Weights of the different kinds of edges
     w_cit = 0.5
     w_key = 0.8
@@ -17,9 +32,25 @@ def main(args):
 
     #Normalization?????
 
+    #TODO: similarity dict initialize
+
     for a in G.nodes_iter():
         # we compute similarity for publications only
         if G.node[a]['Class'] == 'Publication':
+            sim_pub[a] = {}
+            for b in G.nodes_iter():
+                if G.node[b]['Class'] == 'Publication':
+                    #for every two publication check
+                    #whether similarity already computed
+                    try:
+                        exist_sim = sim_pub[b][a] #do nothing
+                    except:
+                        #whether shortest path(a,b) <=t -> otherwise similarity=0, will not be saved
+                        if nx.has_path(G, a, b) and nx.shortest_path_length(G, a, b) <= t:
+                            for path in find_all_paths(G, a, b):
+
+
+
             # compute all paths of the length <= t, with source a
             # for all publication nodes p lying on these paths:
                 # compute sim(a,p) (look up if sim(p,a) already exists)
