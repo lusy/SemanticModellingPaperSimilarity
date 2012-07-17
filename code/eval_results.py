961171f385da4539ca5fd95397093d581c180500
Lusy <vaseva@mi.fu-berlin.de>
2012-07-17 18:38:21 +0200

    eval results imports all data

--- code/eval_results.py
+++ code/eval_results.py
@@ -8,11 +8,24 @@ import networkx as nx
 def main(args):
     # data import
     G = nx.read_graphml('../output/graphml_pubs_till_1975_no_count')
+    param1 = open('../output/output_till_1975_pubs_param1').read()
+    param2 = open('../output/output_till_1975_pubs_param2').read()
+    param3 = open('../output/output_till_1975_pubs_param3').read()
+    crank = open('../output/output_crank_till_1975_no_title').read()
+    
+    p1 = eval(param1)
+    print('Parametrization 1 read')
+    p2 = eval(param2)
+    print('Parametrization 2 read')
+    p3 = eval(param3)
+    print('Parametrization 3 read')
+    cr = eval(crank)
+    print('Crank read')
+
 
     # data structure initialization
     msc_classes = dict()
-
-    #For all MSC Klasses, find out which publication is in class
+        #For all MSC Klasses, find out which publication is in class
     for msc, nbrsdict in G.adjacency_iter():
         if G.node[msc]['Class']=='MSC_Class':
             for pub, eattr in nbrsdict.items():
@@ -23,8 +36,7 @@ def main(args):
                     except:
                         msc_classes[msc] = [pub]
 
-    print ('debuggin.........')                 
-    print ('msc_classes: ', msc_classes)
+    print ('msc_classes initialized')
 
 if __name__ == "__main__":
     t = timeit.Timer("main(args)", "from __main__ import main; args=%r" % sys.argv[1:])
