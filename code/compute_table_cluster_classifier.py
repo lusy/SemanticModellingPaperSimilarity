035d509102ffe448f9f0e00c7bb426a1bce47b80
Lusy <vaseva@mi.fu-berlin.de>
2012-07-19 21:03:07 +0200

    computation of table added

--- code/compute_table_cluster_classifier.py
+++ code/compute_table_cluster_classifier.py
@@ -5,13 +5,36 @@ import re
 sys.path.append("/home/sis/lvaseva/python/lib/python2.6/site-packages")
 
 import networkx as nx
+from numpy import *
 
 def main(args):
     # data import
     G = nx.read_graphml('../data/pubs_till_1975_no_count')
+    clusteringVector #TODO import it
 
+    # computing classifications
     dict_pubs_indices, dict_indices_pubs = map_pubs_to_matrix_indices(G)
-   
+    msc_classes_aggregated, dict_pubs_msc_classes = compute_msc_classes_pubs(G)
+    
+    # map msc classes to some indices
+    dict_msc_classes_to_indices, dict_indices_to_msc_classes = map_msc_classes_to_indices(msc_classes_aggregated)
+       
+    # build table cluster/class
+    evalTable = zeros((65,65))
+
+    for indOfPub in clusteringVector:
+        # clusteringVector[indOfPub] = clusterAssignedToPub
+        pub = dict_indices_pubs[indOfPub]
+
+        # for all classes the publication is in, add 1 to row cluster/class
+        for cl in dict_pubs_msc_classes[pub]:
+            indOfCl = dict_msc_classes_to_indices[cl]
+            evalTable[clusterAssignedToPub-1][indOfCl-1] += 1
+
+    #TODO compute entropy/purity            
+    
+
+def compute_msc_classes_pubs(G)
     #For all MSC Klasses, find out which publication is in class
     msc_classes = dict()
     msc_classes_aggregated = dict()
@@ -67,6 +90,23 @@ def main(args):
         except:
             msc_classes_aggregated['other'].append(p)
             dict_pubs_msc_classes[p] = 'other'
+
+
+    return msc_classes_aggregated, dict_pubs_msc_classes
+
+
+def map_msc_classes_to_indices(input_dict):
+    dict_msc_classes_to_indices = dict()
+    dict_indices_to_msc_classes = dict()
+    ctr = 0
+    
+    for cl in input_dict.keys():
+        ctr = ctr +1
+        dict_msc_classes_to_indices[cl] = ctr
+        dict_indices_to_msc_classes[ctr] = cl
+        
+    return dict_msc_classes_to_indices, dict_indices_to_msc_classes
+ 
 
 
 def map_pubs_to_matrix_indices(g):
