248dfa284c5f0bcff729f5c1a76881d43ad81c17
Lusy <vaseva@mi.fu-berlin.de>
2012-07-19 23:54:27 +0200

    table for cluster classifier is ready

--- code/compute_table_cluster_classifier.py
+++ code/compute_table_cluster_classifier.py
@@ -10,11 +10,15 @@ from numpy import *
 def main(args):
     # data import
     G = nx.read_graphml('../data/pubs_till_1975_no_count')
+
+    # import crank clustering vector as a test
+    f = open("../data/crank_clustering_vector_65_cluster_eval_input")
+    clusteringVector = f.read().splitlines()
     clusteringVector #TODO import it
 
     # computing classifications
     dict_pubs_indices, dict_indices_pubs = map_pubs_to_matrix_indices(G)
-    msc_classes_aggregated, dict_pubs_msc_classes = compute_msc_classes_pubs(G)
+    msc_classes_aggregated, dict_pubs_msc_classes = compute_msc_classes_pubs(G, dict_pubs_indices)
     
     # map msc classes to some indices
     dict_msc_classes_to_indices, dict_indices_to_msc_classes = map_msc_classes_to_indices(msc_classes_aggregated)
@@ -22,19 +26,26 @@ def main(args):
     # build table cluster/class
     evalTable = zeros((65,65))
 
-    for indOfPub in clusteringVector:
+    for indOfPub, clusterAssignedToPub in enumerate(clusteringVector):
         # clusteringVector[indOfPub] = clusterAssignedToPub
-        pub = dict_indices_pubs[indOfPub]
+        pub = dict_indices_pubs[indOfPub+1]
 
         # for all classes the publication is in, add 1 to row cluster/class
-        for cl in dict_pubs_msc_classes[pub]:
+        if type(dict_pubs_msc_classes[pub]) == list :
+            for cl in dict_pubs_msc_classes[pub]:
+                indOfCl = dict_msc_classes_to_indices[cl]
+                evalTable[int(clusterAssignedToPub)-1][indOfCl-1] += 1
+        else:
+            cl = dict_pubs_msc_classes[pub]
             indOfCl = dict_msc_classes_to_indices[cl]
-            evalTable[clusterAssignedToPub-1][indOfCl-1] += 1
+            evalTable[int(clusterAssignedToPub)-1][indOfCl-1] += 1
 
+    print evalTable[0]
+    
     #TODO compute entropy/purity            
     
 
-def compute_msc_classes_pubs(G)
+def compute_msc_classes_pubs(G, dict_pubs_indices):
     #For all MSC Klasses, find out which publication is in class
     msc_classes = dict()
     msc_classes_aggregated = dict()
@@ -66,9 +77,9 @@ def compute_msc_classes_pubs(G)
                         # fill in msc_classes_aggregated
                         try:
                             class_exists = msc_classes_aggregated[first3digits[0]]
-                                # check if publication already belongs to the class
-                                if pub not in msc_classes_aggregated[first3digits[0]]:
-                                    msc_classes_aggregated[first3digits[0]].append(pub)
+                            # check if publication already belongs to the class
+                            if pub not in msc_classes_aggregated[first3digits[0]]:
+                                msc_classes_aggregated[first3digits[0]].append(pub)
                         except:
                             msc_classes_aggregated[first3digits[0]] = [pub]
 
